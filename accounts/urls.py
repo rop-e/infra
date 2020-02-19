bb5d10dcb8ae889bf213605e3413ff63fab3faf0
pixies <pixies@riseup.net>
2020-02-19 11:03:12 -0300

    define routes for urls accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+    login
+)
 
 urlpatterns = [
     path('', index),
+    path('login/', login, name='login'),
+    path('new/', newUser, name='new'),
+    path('logout/', logout, name='logout'),
+    path('reset-password/', resetPassowrd, name='reset-password'),
+    path('profile/', profile, name='profile'),
+
 ]
