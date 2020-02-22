fead81acbb80949252448bb697ad87d0a65aa5fc
pixies <pixies@riseup.net>
2020-02-22 13:28:28 -0300

    add Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    avata = models.ImageField(upload_to='imagens/', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=5000, blank=True)
+
+    def __str__(self):
+        return self.user.username
